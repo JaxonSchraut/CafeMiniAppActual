dc1c3abe732cc8afc4ed52dfd26d30c9e5b45b23
Jaxon Schraut <jqschraut@gmail.com>
2022-09-16 08:06:28 -0500

    09/16

--- CafeMiniAppActual/ViewController.swift
+++ CafeMiniAppActual/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var adminMenuEditField: UITextField!
     @IBOutlet weak var itemSearchField: UITextField!
     
+    @IBOutlet weak var PWStackView: UIStackView!
     @IBOutlet weak var changesStackView: UIStackView!
     
     @IBOutlet weak var menuLabel: UILabel!
@@ -21,18 +22,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalPriceLabel: UILabel!
     
     var menuItems = ["Coffee" : 1.99, "Muffin" : 1.99, "Sandwich" : 4.99]
-    var menuText = "Menu:\nCoffee: $1.99\nMuffin: $1.99\nSandwich: $4.99"
+    var menuText = "Menu:"
+    var total = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for (cafeItem, itemPrice) in menuItems{
+            menuText = "\(menuText)\n\(cafeItem): $\(itemPrice)"
+        }
         menuLabel.text=menuText
+        
+        PWStackView.isHidden=true
         changesStackView.isHidden=true
     }
 
     @IBAction func addItemToCartButton(_ sender: UIButton) {
-        
+        if (itemSearchField.text=="" || itemSearchField.text == "nil"){
+            itemSearchField.text = "You Stupid"
+        } else {
+            
+        }
     }
     
+    @IBAction func adminPopup(_ sender: UIButton) {
+        PWStackView.isHidden=false
+    }
 }
 
